2f1779937e445254676e0189cfc5d3c740ffa75e
albhasan <albhasan@gmail.com>
2020-02-16 22:37:27 -0300

    Linting.

--- mount_esensing.sh
+++ mount_esensing.sh
@@ -7,7 +7,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-if [ $1 = "experiments" ] ; then
+if [ "$1" = "experiments" ] ; then
     sshfs alber@150.163.2.206:/disks/d6/shared/alber/prodes_reproduction                 /home/alber/Documents/data/experiments/prodes_reproduction 
     sshfs alber@150.163.2.206:/disks/d6/shared/alber/l8mod-fusion                        /home/alber/Documents/data/experiments/l8mod-fusion
     #sshfs alber@150.163.2.206:/home/alber/Documents/data/experiments/amazon_degradation /home/alber/Documents/data/experiments/amazon_degradation
@@ -16,7 +16,7 @@ if [ $1 = "experiments" ] ; then
 fi
 
 
-if [ $1 = "data" ] ; then
+if [ "$1" = "data" ] ; then
     # sshfs -o ro alber@150.163.2.208:/net/150.163.2.208/disks/d7/hdf2tif_03            /home/alber/Documents/data/e-sensing/hdf2tif_03
     # sshfs -o ro alber@150.163.2.206:/disks/d6/shared/alber/prodes_mosaic/               /home/alber/prodes_mosaic
     sshfs alber@150.163.2.206:/disks/d6/shared/alber                                    /home/alber/shared
