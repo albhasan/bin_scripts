ab60f0824de10fedf3042ed62744c161ede353a1
albhasan <albhasan@gmail.com>
2020-02-16 22:34:27 -0300

    Linting

--- isLANDSATdownloading.sh
+++ isLANDSATdownloading.sh
@@ -3,18 +3,18 @@
 # CHECK IF THE DOWNLOAD SCRIPT IS RUNNING
 ################################################################################
 
-result=`pgrep -fl download_espa_order.py | wc -l`
+result=$(pgrep -fl download_espa_order.py | wc -l)
 scriptpath="$( cd "$(dirname "$0")" ; pwd -P )"
-scriptname=`basename "$0"`
+scriptname=$(basename "$0")
 thetime="$(date '+%Y-%m-%d %H:%M:%S')"
-if [ $result -ge 1 ]
+if [ "$result" -ge 1 ]
     then
     echo "isLANDSATdownloading.sh is downloading images"
 else
     echo "$thetime: isLANDSATdownloading is not running. Starting it...." >> "$scriptpath/$scriptname".log
     python /home/alber/Documents/ghProjects/espa-bulk-downloader/download_espa_order.py -o ALL -d /home/alber/Documents/data/LANDSAT/orders -u albhasan -p WU3o1VxrnpEcvy6q -v -c &
-    imagepart=`find /home/alber/Documents/data/LANDSAT/orders -name "*part*" | wc -l`
-    if [ $imagepart -ge 1 ]
+    imagepart=$(find /home/alber/Documents/data/LANDSAT/orders -name "*part*" | wc -l)
+    if [ "$imagepart" -ge 1 ]
         then
         echo "$thetime: There are images partially downloaded"
     else
