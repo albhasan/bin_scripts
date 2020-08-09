53c4070cd1c0529ebfeb317495d322ec1c493973
--global <albhasan@gmail.com>
2020-08-08 21:07:30 -0300

    Switched to cryfs

--- mount_dropbox_dirs.sh
+++ mount_dropbox_dirs.sh
@@ -4,28 +4,22 @@ machine=$(hostname)
 echo "Mounting directories on $machine..."
 
 if [[ "$machine" == "averno" ]]; then
-    db_path="/media/alber/fd2f2147-f07d-4a8a-9e17-715140f0e470/Dropbox"
-    mount --bind /media/alber/fd2f2147-f07d-4a8a-9e17-715140f0e470            /home/alber/Documents
-    mount --bind /media/alber/fd2f2147-f07d-4a8a-9e17-715140f0e470/mp3        /home/alber/Music
+    cryfs ~/Dropbox/dropbox_encrypted ~/dropbox_decrypted
 elif [[ "$machine" == "darcy" ]]; then
-    db_path="/home/alber/Dropbox"
+    cryfs ~/Dropbox/dropbox_encrypted ~/dropbox_decrypted
 elif [[ "$machine" == "sdb-desktop" ]]; then
-    db_path=" /home/alber/Documents/Dropbox"
-    mount --bind /home/alber/Documents/mp3                                    /home/alber/Music
+    cryfs ~/Documents/Dropbox/dropbox_encrypted ~/dropbox_decrypted
 else
     echo "ERROR: Unknown machine!"
     exit 1
 fi
-runuser -l  alber -c "encfs $db_path/encrypted ~/dbox-encrypted"
-mount --bind "$db_path"/Photos                                                /home/alber/Pictures
 
 ##########################################################
-# ENCRYPT DROPBOX
+# https://www.cryfs.org/tutorial/
+# sudo apt install cryfs
+# cryfs ~/Dropbox/dropbox_encrypted ~/dropbox_decrypted
+##########################################################
+# OLD ENCRYPT DROPBOX
 # http://www.howtogeek.com/121737/how-to-encrypt-cloud-storage-on-linux-and-windows-with-encfs/
-# DO NOT WRITE TO THE DROPBOX FOLDER, USE THE OTHER FOLDER INSTEAD
 ##########################################################
-# apt-get install encfs
 
-# mount each time
-# encfs /media/alber/fd2f2147-f07d-4a8a-9e17-715140f0e470/Dropbox/encrypted ~/dbox-encrypted 	# averno
-# runuser -l  alber -c 'encfs /media/alber/fd2f2147-f07d-4a8a-9e17-715140f0e470/Dropbox/encrypted ~/dbox-encrypted'
